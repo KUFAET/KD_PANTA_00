963045feb6615086384e3e5c143530cc3e0e1cf3
KUFAET <kufaethan12@gmail.com>
2024-12-08 22:52:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "y3IRDSxJ#qQVDZroRpv6MWOgOkncqChAFde4O7UN1hM9S4P1Ks1c",
+    SESSION_ID: process.env.SESSION_ID || "DvR1zI5C#uS-uiirC7bgVknRkj24pYAHMeDCEXA54zWq6n3iKfU8",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
